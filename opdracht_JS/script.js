a7fa60907ea8c1ab0f4fbad46d5c8e0f94601c0a
joedevcode <58709729+joedevcode@users.noreply.github.com>
2021-05-26 16:27:58 +0200

    Add files via upload

---
+++ opdracht_JS/script.js
@@ -0,0 +1,11 @@
+let abc = "Joe";
+console.log("Hello Winc Academy");
+console.log(abc);
+console.log(4 + 4);
+console.log(4 - 4);
+console.log(4 * 4);
+console.log(4 / 4);
+console.log(4 % 4);
+
+let myAge = 48;
+console.log(typeof myAge);
