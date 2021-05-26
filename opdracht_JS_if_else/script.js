e17cf975b590bf98eb00826510ace84110a6115e
joedevcode <58709729+joedevcode@users.noreply.github.com>
2021-05-27 01:55:39 +0200

    Add files via upload

---
+++ opdracht_JS_if_else/script.js
@@ -0,0 +1,28 @@
+console.log("Hallo Winc Academy Studenten");
+
+/*
+// Dit is een grote som
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+*/
+
+const age = 23;
+if (age >= 18) {
+    console.log("Please enter in 5 minutes");
+} else {
+    console.log("Please wait outside");
+}
+
+const isFemale = true;
+if (isFemale == true) {
+    console.log("It's a female.");
+} else {
+    console.log("It's NOT a female.");
+}
+
+const driverStatus = 'bob';
+if (driverStatus == 'bob') {
+    console.log("You may drive.");
+} else {
+    console.log("You may not drive.");
+}
