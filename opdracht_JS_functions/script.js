2f5a44dd82a356724f3413b70804888d90ca339b
joedevcode <58709729+joedevcode@users.noreply.github.com>
2021-05-27 21:07:10 +0200

    Add files via upload

---
+++ opdracht_JS_functions/script.js
@@ -0,0 +1,7 @@
+const makingPizza = function () {
+    console.log("Prepare your ingredients");
+    console.log("Do the workshop");
+    console.log("Put the pizza in the oven");
+}
+
+makingPizza();
