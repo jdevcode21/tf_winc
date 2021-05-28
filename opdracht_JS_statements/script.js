d886fc4c55132e23166641b661e19a2d2e8577f5
joedevcode <58709729+joedevcode@users.noreply.github.com>
2021-05-28 02:46:57 +0200

    Update script.js

--- opdracht_JS_statements/script.js
+++ opdracht_JS_statements/script.js
@@ -10,30 +10,24 @@ console.log(checker(100));
 
 // Bouncer at a club  
 const pplChecker = function (maxNum, currNum, age) {
-    let thingsBrendaSay = [];
-    if (maxNum == 100) {
-        return thingsBrendaSay.push("it's too busy now, come back later");
+    if (currNum >= maxNum) {
+        return "it's too busy now, come back later";
     }
-    if (currNum < 100) {
-        return thingsBrendaSay.push("come in");
-    }
-    if (age <= 18) {
-        return thingsBrendaSay.push("this is a club for adults");
+    if (age < 18) {
+        return "this is a club for adults";
     } else {
-        return thingsBrendaSay
+        return "come in";
     }
 };
 
 console.log(pplChecker(100, 90, 18));
 
+
 // Calculating the average
 const calAverage = function (num1, num2, num3, num4, num5) {
     let sum = num1 + num2 + num3 + num4 + num5;
     let average = sum / 5;
-    let outcome = Math.round(average);
-    return outcome;
-}
+    return average;
+};
 
-console.log(outcome);
-
-calAverage(1, 2, 3, 4, 5);
+console.log(calAverage(1, 2, 3, 4, 5));
